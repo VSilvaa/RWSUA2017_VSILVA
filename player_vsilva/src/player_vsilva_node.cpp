61379401fb01d868d9b9ccbddfe7efbc0a26018f
VSilvaa <vans@ua.pt>
2017-03-10 11:16:55 +0000

    msgs + libs

--- player_vsilva/src/player_vsilva_node.cpp
+++ player_vsilva/src/player_vsilva_node.cpp
@@ -8,56 +8,14 @@
 
 #include <iostream>  
 #include <vector> 
+#include <rwsua2017_libs/player.h>
+#include <rwsua2017_msgs/MakeAPlay.h>
+
                          
 using namespace std; 
                                                          
 namespace rwsua2017
 {
-
-class Player
-{
-    public:
-
-    Player(string argin_name, string argin_team_name="blue") //Mesmo nome porque é construtora, corre quando se chama ~
-    {
-        this->name = name;
-	set_team_name(argin_team_name);
-    }
-
-
-    string name;
-
-	//accessor
-	void set_team_name(string argin_team_name)
-	{
-		if( argin_team_name=="red" || argin_team_name=="blue" || argin_team_name=="green" )
-		{
-			this->team_name = argin_team_name;
-		}
-		else 
-		{
-			cout << "ERRO no nome!" << endl;
-		}
-	}
-
-	void set_team_name(void)
-	{
-		set_team_name("red");
-	}
-
-	string get_team_name(void)	
-	{
-		return this->team_name;
-	}
-
-    private:
-
-	string team_name;
-
-
-
-};
-
 
 
 class MyPlayer:public Player
