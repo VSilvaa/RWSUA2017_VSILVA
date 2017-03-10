a29600a81a6e98148845f6599f695117f56334d6
VSilvaa <vans@ua.pt>
2017-03-10 15:09:45 +0000

    asd

--- player_vsilva/src/player_vsilva_node.cpp
+++ player_vsilva/src/player_vsilva_node.cpp
@@ -8,38 +8,50 @@
 
 #include <iostream>  
 #include <vector> 
+#include <ros/ros.h>
 #include <rwsua2017_libs/player.h>
 #include <rwsua2017_msgs/MakeAPlay.h>
 
                          
-using namespace std; 
+using namespace std;
+using namespace boost;
                                                          
 namespace rwsua2017
 {
 
 
-class MyPlayer:public Player
-{
-    public: 
-
-    MyPlayer(string argin_name, string argin_team_name): Player(argin_name,argin_team_name)
-    {
-        //setTeamName(team);
-	cout << "MyPlayer" << endl;
-    }
-
-	vector<string> teammates;
-
-};
+	class MyPlayer:public Player
+	{
+		ros::Subscriber sub;
+	    public: 
+		
 
 
+	    MyPlayer(string argin_name, string argin_team_name): Player(argin_name,argin_team_name)
+	    {
+
+		sub = n.subscribe("/make_a_play", 1000, &MyPlayer::make_plays_callback,this);
+
+		//setTeamName(team);
+		cout << "MyPlayer" << endl;
+	    }
+
+		vector<string> teammates;
+
+	void make_plays_callback(const rwsua2017_msgs::MakeAPlay::ConstPtr& msg)
+	{
+	  cout << "Coiso!! with max disp.= "<< msg->max_displacement << endl;
+	}
+
+
+	};
 
 }
 
-
-
-int main()
+int main(int argc, char **argv)
 {
+
+	ros::init(argc, argv, "listener");
 
     string player_name = "vsilva";
     //Creating an instance of class Player
@@ -56,6 +68,8 @@ int main()
 	{
 		cout << myplayer.teammates[i] << endl;
 	}
+	
+	ros::spin();
 
 	return 1;
 
